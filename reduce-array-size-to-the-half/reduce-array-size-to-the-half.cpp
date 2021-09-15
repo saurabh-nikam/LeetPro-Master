4f35de448e0fe29e3c9651bc9959af316f8c4283
SAURABH NIKAM <52209941+saurabh-nikam@users.noreply.github.com>
2021-09-15 09:09:35 +0530

    Time: 248 ms (36.06%), Space: 78.4 MB (64.72%) - LeetHub

--- reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
+++ reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int minSetSize(vector<int>& arr) {
+   int minSetSize(vector<int>& arr) {
     unordered_map<int, int> m;
-    multiset<int, greater <int>> s;        
+    priority_queue<int> pq;
     for (auto n : arr) ++m[n];
-    for (auto &p : m) s.insert(p.second);
+    for (auto &p : m) pq.push(p.second);
     int res = 0, cnt = 0;
-    for (auto it = begin(s); cnt * 2 < arr.size(); ++it) {
+    while (cnt * 2 < arr.size()) {
         ++res;
-        cnt += *it;
+        cnt += pq.top(); pq.pop();
     }
     return res;
 }
