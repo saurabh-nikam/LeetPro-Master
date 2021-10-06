6abd5de33c6c873c4bd531660e843a19f08a6afb
SAURABH NIKAM <52209941+saurabh-nikam@users.noreply.github.com>
2021-10-06 21:55:44 +0530

    Time: 20 ms (59.64%), Space: 17.9 MB (63.63%) - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
+    void sort(vector<int>& arr)
+    {
+        int n = arr.size();
+        int i = 0;
+        while(i < n)
+        {
+            int correct = arr[i];
+            if(correct < n && arr[i] != arr[correct])
+                swap(arr[i],arr[correct]);
+            else 
+                i++;
+        }
+    }
     int missingNumber(vector<int>& nums) {
-        int sum = 0;
         int n = nums.size();
-        for(auto &i : nums)
-            sum+=i;
+        sort(nums);
+        for(int i = 0;i<n;i++)
+        {
+            if(nums[i] != i)
+                return i;
+        }
+        return n;
         
-        return (n)*(n+1)/2 - sum;
         
     }
 };
