73f55250ca2c6e02cd36caba3ec9fb20515f1747
Saurabh Nikam <52209941+saurabh-nikam@users.noreply.github.com>
2022-01-24 11:31:38 +0530

    Time: 32 ms (75.43%), Space: 14.3 MB (46.33%) - LeetHub

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -1,4 +1,4 @@
-class Solution(object):
-   def detectCapitalUse(self, word):
-    return word[1:]==word[1:].lower() or word==word.upper()
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return word[1:]==word[1:].lower() or word==word.upper()
         
