ebbd8c0659d505e67d6b62340006decbe1030e37
Saurabh Nikam <52209941+saurabh-nikam@users.noreply.github.com>
2022-03-23 14:01:01 +0530

    Time: 31 ms (37.50%), Space: 13.2 MB (100.00%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.py
@@ -0,0 +1,9 @@
+class Solution(object):
+       def brokenCalc(self, X, Y):
+            res = 0
+            while X < Y:
+                res += Y % 2 + 1
+                Y = (Y + 1) / 2
+            return res + X - Y
+
+        
