9da30c66d77a8b4b7c880e474343cbf11544ff45
Saurabh Nikam <52209941+saurabh-nikam@users.noreply.github.com>
2022-08-29 17:40:05 +0530

    Time: 59 ms (46.36%), Space: 12.1 MB (97.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& p,int i,int j)
+    {
+        if(i<0 || j<0 || i>=p.size() || j>=p[0].size() || p[i][j]=='0')
+            return;
+        p[i][j]='0';
+        dfs(p,i-1,j);
+        dfs(p,i+1,j);
+        dfs(p,i,j-1);
+        dfs(p,i,j+1);
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        vector<vector<bool>> visit(grid.size(),vector<bool>(grid[0].size(),false));
-        queue<pair<int,int>> q;
- 
-        for(int  i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-             if(visit[i][j] || grid[i][j]=='0')continue;
-                q.push({i,j});
-                count++;
-                while(q.size()){
-                    int u = q.front().first;
-                    int v = q.front().second;
-                    q.pop();
-                    if(u>=grid.size() || u<0 || v>=grid[0].size() || v<0 || visit[u][v] || grid[u][v]=='0')continue;
-                    visit[u][v] = true; 
-                   q.push({u+1,v});
-                   q.push({u-1,v});
-                   q.push({u,v+1});
-                   q.push({u,v-1}); 
-                }  
-            }   
-        }
-        
-
-        return count;
-        
+        int ans=0;
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[i].size();j++)
+                if(grid[i][j]=='1')
+                {
+                    dfs(grid,i,j);
+                    ans++;
+                }
+        return ans;
     }
 };
